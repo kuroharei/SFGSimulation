c9c00cbb491e25757c98d70906adf7513a35a9f3
Kuroha Rei <kuroharei.xyz@gmail.com>
2023-10-11 18:32:37 +0800

    update

--- frontend/src/Components/APIService.js
+++ frontend/src/Components/APIService.js
@@ -1,14 +1,14 @@
 export class APIService {
     static Calculate(data) {
-        return  fetch('http://0.0.0.0:5000/calculation', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': '*'
-                    },
-                    body: JSON.stringify(data)
-                })
-                .then(resp => resp.json())
+        return fetch('http://111.229.185.54:5000//calculation', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(resp => resp.json())
     }
 }
 
