709bced9102e946672805218a108a1d00f990237
Kuroha Rei <kuroharei.xyz@gmail.com>
2023-10-17 20:41:21 +0800

    update

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -3,25 +3,22 @@ import LAB_1 from '../images/LAB-1.jpg'
 
 function Homepage({setPage}) {
   return (
-    <div className="container-md">
-        {/* <div className='row'>
-          <img src={LAB_1} className='img-fluid' alt='...' />
-        </div> */}
-        <h1 className='text-center text-white shadow-lg mt-3 mb-3' style={{fontSize: `calc(1.5em + 1vw)`}}>Hongfei Wang's Group</h1>
-        <div className='row justify-content-center'>
+    <div className="container-md" style={{minWidth: `50vw`}}>
+        <h1 className='text-center text-white shadow-lg mt-3 mb-3' style={{fontSize: `calc(1em + 1.5vw)`}}>Hongfei Wang's Group</h1>
+        <div className='row justify-content-center' style={{minHeight: `80vh`}}>
           <div className='card'>
               <div className='card-header'>
-                <h3 className='display-5 text-monospace' style={{fontSize: `calc(1em + 1vw)`}}>Ultrafast Laser Spectroscopy and Surface/Interface Physical Chemistry Laboratory</h3>
-                <p className='display-5 text-monospace' style={{fontSize: `calc(0.5em + 1vw)`}}>School of Science, Westlake University</p>
+                <h3 className='display-5 text-monospace font-weight-bolder' style={{fontSize: `calc(1em + (1vw + 2.5vh) / 2)`}}>Ultrafast Laser Spectroscopy and Surface/Interface Physical Chemistry Laboratory</h3>
+                <p className='display-5 text-monospace' style={{fontSize: `calc((1vw + 2.5vh) / 2)`}}>School of Science, Westlake University</p>
               </div>
               <div className='card-body'>
-                <p className='text-justify text-lg-left text-monospace' style={{fontSize: `calc(0.5em + 1vw)`}}>
+                <p className='text-justify text-lg-left text-monospace' style={{fontSize: `calc((1vw + 2.5vh) / 2)`, lineHeight: `calc(6vh)`}}>
                   Our research group intends to carry out long-term research on modern physical chemistry and chemical physics related to surface/interface and condensed phase ultrafast and nonlinear spectra and dynamics, including:<br/>
                   <ol className='mt-3'>
-                    <li className='mb-3'>Physical chemistry of surfaces and interfaces; structure, interaction and chemical dynamics</li>
-                    <li className='mb-3'>Spectroscopy and molecular reaction dynamics: vibration and electronic spectroscopy</li>
-                    <li className='mb-3'>Linear and nonlinear, spectroscopy and modern spectroscopic analysis methods : Infrared, Raman and sum frequency spectroscopy</li>
-                    <li className='mb-3'>Chirality and stereochemistry on surfaces and biofilms.</li>
+                    <li>Physical chemistry of surfaces and interfaces; structure, interaction and chemical dynamics</li>
+                    <li>Spectroscopy and molecular reaction dynamics: vibration and electronic spectroscopy</li>
+                    <li>Linear and nonlinear, spectroscopy and modern spectroscopic analysis methods : Infrared, Raman and sum frequency spectroscopy</li>
+                    <li>Chirality and stereochemistry on surfaces and biofilms.</li>
                   </ol>
                 </p>
               </div>
