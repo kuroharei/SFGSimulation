f69b92fddc3ea1df7b9f8df1a30e51eb5649a1f2
kuroharei <kuroharei.xyz@gmail.com>
2023-11-29 09:58:19 +0800

    update

--- backend/app.py
+++ backend/app.py
@@ -189,5 +189,5 @@ def calculation():
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=5000)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(debug=True)
